7774dcf1e1b38ba5267d93387d8f5c993b53a8ed
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-08-10 19:38:36 +0300

    read only api is ready, next crud

---
+++ base/views.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+
+def drink_list(request):
+    
+    #get all the drinks
+    #serialize them
+    #return json
+    drinks = Drink.objects.all()
+    serializer = DrinkSerializer(drinks, many=True)
+    return JsonResponse(serializer.data, safe=False)
+    
+    
+
+# Create your views here.
